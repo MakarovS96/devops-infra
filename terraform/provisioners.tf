375c3793176a919d68b88b744fb365f53149bc15
Sam Gamgee <smakalist@gmail.com>
2024-05-28 10:29:33 +0400

    added ansible plays for setup k8s cluster

--- terraform/provisioners.tf
+++ terraform/provisioners.tf
@@ -9,99 +9,42 @@ resource "local_file" "create_inventory" {
     })
 }
 
-/*resource "null_resource" "docker-swarm-manager-init" {
-    depends_on = [ yandex_compute_instance.manager ]
+resource "null_resource" "wait_for_instances" {
+    depends_on = [ yandex_compute_instance.vm, local_file.create_inventory]
+    count = var.instances_count
 
     connection {
         type = "ssh"
-        host = yandex_compute_instance.manager.network_interface[0].nat_ip_address
-        user = var.ssh_user.name
-        private_key = file(var.ssh_user.private_key)
-    }
-
-    provisioner "file" {
-      source = "../docker-compose.yml"
-      destination = "docker-compose.yml"
-    }
-
-    provisioner "local-exec" {
-        command = "ansible-playbook -u sennin -i '${yandex_compute_instance.manager.network_interface[0].nat_ip_address},' --private-key ${var.ssh_user.private_key} ansible/docker-install-playbook.yml"
-    }
-
-    provisioner "local-exec" {
-        command = "ansible-playbook -u sennin -i '${yandex_compute_instance.manager.network_interface[0].nat_ip_address},' --private-key ${var.ssh_user.private_key} ansible/docker-swarm-manager-playbook.yml"
-    }
-}
-
-resource "null_resource" "docker-swarm-worker-get-token" {
-    depends_on = [ yandex_compute_instance.manager, null_resource.docker-swarm-manager-init ]
-    connection {
-        type = "ssh"
-        host = yandex_compute_instance.manager.network_interface[0].nat_ip_address
-        user = var.ssh_user.name
-        private_key = file(var.ssh_user.private_key)
-    }
-
-    provisioner "local-exec" {
-        command = "echo $(ssh -i ${var.ssh_user.private_key} -o StrictHostKeyChecking=no ${var.ssh_user.name}@${yandex_compute_instance.manager.network_interface[0].nat_ip_address} docker swarm join-token -q worker) > keys/swarm_token"
-    }
-}
-
-resource "local_file" "docker-swarm-worker-ansible" {
-  depends_on = [ null_resource.docker-swarm-worker-get-token, data.local_file.swarm_token ]
-  filename = "ansible/docker-swarm-worker-playbook.yml"
-  content = templatefile("ansible/docker-swarm-worker-playbook.yml.tmpl", {"address": yandex_compute_instance.manager.network_interface[0].ip_address, "swarm_token": trimspace(data.local_file.swarm_token.content)})
-}
-
-resource "null_resource" "docker-swarm-worker1-join" {
-    depends_on = [ yandex_compute_instance.worker1, local_file.docker-swarm-worker-ansible ]
-    connection {
-        type = "ssh"
-        host = yandex_compute_instance.worker1.network_interface[0].nat_ip_address
-        user = var.ssh_user.name
-        private_key = file(var.ssh_user.private_key)
-    }
-
-    provisioner "local-exec" {
-        command = "ansible-playbook -u sennin -i '${yandex_compute_instance.worker1.network_interface[0].nat_ip_address},' --private-key ${var.ssh_user.private_key} ansible/docker-install-playbook.yml"
-    }
-
-    provisioner "local-exec" {
-        command = "ansible-playbook -u sennin -i '${yandex_compute_instance.worker1.network_interface[0].nat_ip_address},' --private-key ${var.ssh_user.private_key} ansible/docker-swarm-worker-playbook.yml"
-    }
-}
-
-resource "null_resource" "docker-swarm-worker2-join" {
-    depends_on = [ yandex_compute_instance.worker1, local_file.docker-swarm-worker-ansible ]
-    connection {
-        type = "ssh"
-        host = yandex_compute_instance.worker2.network_interface[0].nat_ip_address
-        user = var.ssh_user.name
-        private_key = file(var.ssh_user.private_key)
-    }
-
-    provisioner "local-exec" {
-        command = "ansible-playbook -u sennin -i '${yandex_compute_instance.worker2.network_interface[0].nat_ip_address},' --private-key ${var.ssh_user.private_key} ansible/docker-install-playbook.yml"
-    }
-
-    provisioner "local-exec" {
-        command = "ansible-playbook -u sennin -i '${yandex_compute_instance.worker2.network_interface[0].nat_ip_address},' --private-key ${var.ssh_user.private_key} ansible/docker-swarm-worker-playbook.yml"
-    }
-}
-
-resource "null_resource" "docker-swarm-manager-start" {
-    depends_on = [ yandex_compute_instance.manager, null_resource.docker-swarm-worker1-join, null_resource.docker-swarm-worker2-join ]
-
-    connection {
-        type = "ssh"
-        host = yandex_compute_instance.manager.network_interface[0].nat_ip_address
+        host = yandex_compute_instance.vm[count.index].network_interface[0].nat_ip_address
         user = var.ssh_user.name
         private_key = file(var.ssh_user.private_key)
     }
 
     provisioner "remote-exec" {
-        inline = [
-            "docker stack deploy --compose-file docker-compose.yml sockshop-swarm"
-         ]
+      inline = [ "echo Works!" ]
     }
-}*/
+}
+
+resource "null_resource" "run_k8s_config" {
+    depends_on = [ null_resource.wait_for_instances ]
+
+    provisioner "local-exec" {
+        command = "ansible-playbook -u sennin -i ../ansible/inventory --private-key ${var.ssh_user.private_key} ../ansible/preconf-play.yml"
+    }
+}
+
+resource "null_resource" "run_master_node_config" {
+    depends_on = [ null_resource.run_k8s_config ]
+
+    provisioner "local-exec" {
+        command = "ansible-playbook -u sennin -i ../ansible/inventory --private-key ${var.ssh_user.private_key} ../ansible/conf-k8s-manager-play.yml"
+    }
+}
+
+resource "null_resource" "run_workers_node_config" {
+    depends_on = [ null_resource.run_k8s_config ]
+
+    provisioner "local-exec" {
+        command = "ansible-playbook -u sennin -i ../ansible/inventory --private-key ${var.ssh_user.private_key} ../ansible/conf-k8s-workers-play.yml"
+    }
+}
