df75654ffcfdffac5f5a56e680ea618fdc6f74b5
Sam Gamgee <smakalist@gmail.com>
2024-06-02 23:40:19 +0400

    removed hardcoded user name and added ingerss controller

--- terraform/provisioners.tf
+++ terraform/provisioners.tf
@@ -45,7 +45,7 @@ resource "null_resource" "run_preconf" {
     depends_on = [ null_resource.wait_for_cluster_instances, null_resource.wait_for_srv_instance ]
 
     provisioner "local-exec" {
-        command = "ansible-playbook -u sennin -i ../ansible/inventory --private-key ${var.ssh_user.private_key} ../ansible/preconf-all-play.yml"
+        command = "ansible-playbook -u ${var.ssh_user.name} -i ../ansible/inventory --private-key ${var.ssh_user.private_key} ../ansible/preconf-all-play.yml"
     }
 }
 
@@ -59,7 +59,7 @@ resource "null_resource" "run_srv_config" {
           GITHUB_REPO = var.github_data.repo
           GITHUB_ACCOUNT = var.github_data.account
         }
-        command = "ansible-playbook -u sennin -i ../ansible/inventory --private-key ${var.ssh_user.private_key} ../ansible/preconf-srv-play.yml"
+        command = "ansible-playbook -u ${var.ssh_user.name} -i ../ansible/inventory --private-key ${var.ssh_user.private_key} ../ansible/preconf-srv-play.yml"
     }
 }
 
@@ -67,7 +67,7 @@ resource "null_resource" "run_k8s_config" {
     depends_on = [ null_resource.run_preconf ]
 
     provisioner "local-exec" {
-        command = "ansible-playbook -u sennin -i ../ansible/inventory --private-key ${var.ssh_user.private_key} ../ansible/preconf-cluster-play.yml"
+        command = "ansible-playbook -u ${var.ssh_user.name} -i ../ansible/inventory --private-key ${var.ssh_user.private_key} ../ansible/preconf-cluster-play.yml"
     }
 }
 
@@ -75,15 +75,36 @@ resource "null_resource" "run_master_node_config" {
     depends_on = [ null_resource.run_k8s_config ]
 
     provisioner "local-exec" {
-        command = "ansible-playbook -u sennin -i ../ansible/inventory --private-key ${var.ssh_user.private_key} ../ansible/conf-k8s-manager-play.yml"
+        command = "ansible-playbook -u ${var.ssh_user.name} -i ../ansible/inventory --private-key ${var.ssh_user.private_key} ../ansible/conf-k8s-manager-play.yml"
     }
+}
+
+resource "null_resource" "copy_kubeconfig" {
+
+    depends_on = [ null_resource.run_master_node_config ]
+
+    connection {
+        type = "ssh"
+        host = yandex_compute_instance.vm[0].network_interface[0].nat_ip_address
+        user = var.ssh_user.name
+        private_key = file(var.ssh_user.private_key)
+    }
+
+    provisioner "local-exec" {
+      command = "scp -i ${var.ssh_user.private_key} ${var.ssh_user.name}@${yandex_compute_instance.vm[0].network_interface[0].nat_ip_address}:~/.kube/config ../.keys/kubeconfig"
+    }
+
+    provisioner "local-exec" {
+      command = "sed -i '' 's/${yandex_compute_instance.vm[0].network_interface[0].ip_address}/${yandex_compute_instance.vm[0].network_interface[0].nat_ip_address}/g' '../.keys/kubeconfig'"
+    }
+
 }
 
 resource "null_resource" "run_workers_node_config" {
     depends_on = [ null_resource.run_master_node_config ]
 
     provisioner "local-exec" {
-        command = "ansible-playbook -u sennin -i ../ansible/inventory --private-key ${var.ssh_user.private_key} ../ansible/conf-k8s-workers-play.yml"
+        command = "ansible-playbook -u ${var.ssh_user.name} -i ../ansible/inventory --private-key ${var.ssh_user.private_key} ../ansible/conf-k8s-workers-play.yml"
     }
 }
 
@@ -107,6 +128,6 @@ resource "null_resource" "clear_gh_runners" {
             GITHUB_REPO = self.triggers.repo
             GITHUB_ACCOUNT = self.triggers.account
         }
-        command = "ansible-playbook -u sennin -i ../ansible/inventory --private-key ${self.triggers.private_key} ../ansible/on-destroy-srv-play.yml"
+        command = "ansible-playbook -u ${self.triggers.user} -i ../ansible/inventory --private-key ${self.triggers.private_key} ../ansible/on-destroy-srv-play.yml"
     }
 }
